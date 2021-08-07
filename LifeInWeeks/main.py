0a2c4e680220b5d5aa1da1b504eef413e3541e86
Adam Melancon <adammelancon@gmail.com>
2021-08-07 05:16:14 +0000

    add

---
+++ LifeInWeeks/main.py
@@ -0,0 +1,18 @@
+age = input("What is your current age?")
+
+age = int(age)
+dead_in = 90 - age
+
+days = dead_in * 365
+weeks = dead_in * 52
+months = dead_in * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
+
+
+
+
+
+
+
