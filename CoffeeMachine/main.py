4f3f564051ae7438e159a3336cae99ef452eb80c
Adam Melancon <adammelancon@gmail.com>
2021-09-02 01:50:00 +0000

    Added Day 19 - Hirst Painting

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -43,10 +43,12 @@ resources = {
 # }
 # ========================================================================
 
+
 def round_money(x):
     '''rounds money to two decimals with trailing zeros'''
     x = '{:.2f}'.format(round(float(x), 2))
     return x
+
 
 def clear_screen():
     '''Clears the screen'''
@@ -60,13 +62,16 @@ def clear_screen():
 def error_msg(em):
     '''Takes either ("no_resources") or ("not_enough_money") to print error message for each.'''
     if em == "no_resources":
-        print("      Not enough resources. \nPlease choose something different.")
+        print(
+            "      Not enough resources. \nPlease choose something different.")
         print("")
         print("-----------Press Enter To Continue----------------")
         input("")
         take_order()
     if em == "not_enough_money":
-        print("      You Didn't Enter Enough Money. \nPlease choose something different.")
+        print(
+            "      You Didn't Enter Enough Money. \nPlease choose something different."
+        )
         print("")
         print("-----------Press Enter To Continue----------------")
         input("")
@@ -97,15 +102,19 @@ def add_resources():
     print(f"Coffee: {resources['coffee']} g")
     print(f" Money: ${round_money(resources['money'])}")
     print("--------------------------------------------------")
-    add_what = input("What are you adding? [W]ater / [M]ilk / [C]offee / [E]xit: ").lower()
+    add_what = input(
+        "What are you adding? [W]ater / [M]ilk / [C]offee / [E]xit: ").lower()
     if add_what == "w":
-        resources['water'] = resources['water'] + int(input("Enter water amount in ml: "))
+        resources['water'] = resources['water'] + int(
+            input("Enter water amount in ml: "))
         add_resources()
     elif add_what == "m":
-        resources['milk'] = resources['milk'] + int(input("Enter milk amount in ml: "))
+        resources['milk'] = resources['milk'] + int(
+            input("Enter milk amount in ml: "))
         add_resources()
     elif add_what == "c":
-        resources['coffee'] = resources['coffee'] + int(input("Enter coffee amount in ml: "))
+        resources['coffee'] = resources['coffee'] + int(
+            input("Enter coffee amount in ml: "))
         add_resources()
     elif add_what == "e":
         take_order()
@@ -114,21 +123,28 @@ def add_resources():
 def check_resources(drink_choice):
     '''Takes the drink choice (e, l, or c)'''
     if drink_choice == 'e':
-        if MENU['espresso']["ingredients"]['water'] > resources["water"] or MENU['espresso']["ingredients"]['coffee'] > resources["coffee"]:
+        if MENU['espresso']["ingredients"]['water'] > resources[
+                "water"] or MENU['espresso']["ingredients"][
+                    'coffee'] > resources["coffee"]:
             error_msg("no_resources")
         else:
             print("=================")
             print("Espresso Chosen")
             process_payment(drink_choice)
     elif drink_choice == 'l':
-        if MENU['latte']["ingredients"]['water'] > resources["water"] or MENU['latte']["ingredients"]['coffee'] > resources["coffee"] or MENU['latte']["ingredients"]['milk'] > resources["milk"]:
+        if MENU['latte']["ingredients"]['water'] > resources["water"] or MENU[
+                'latte']["ingredients"]['coffee'] > resources["coffee"] or MENU[
+                    'latte']["ingredients"]['milk'] > resources["milk"]:
             error_msg("no_resources")
         else:
             print("=================")
             print("Latte Chosen")
             process_payment(drink_choice)
     elif drink_choice == 'c':
-        if MENU['cappuccino']["ingredients"]['water'] > resources["water"] or MENU['cappuccino']["ingredients"]['coffee'] > resources["coffee"] or MENU['cappuccino']["ingredients"]['milk'] > resources["milk"]:
+        if MENU['cappuccino']["ingredients"]['water'] > resources[
+                "water"] or MENU['cappuccino']["ingredients"][
+                    'coffee'] > resources["coffee"] or MENU['cappuccino'][
+                        "ingredients"]['milk'] > resources["milk"]:
             error_msg("no_resources")
         else:
             print("=================")
@@ -183,7 +199,6 @@ def process_payment(which_drink):
         print("Thanks!")
 
 
-
 def take_money():
     '''Accepts money from customer and returns how much they put in the machine.'''
     global resources
@@ -199,10 +214,11 @@ def take_money():
     clear_screen()
     # Calculate total money given and add to register.
     total_pay = quarters + dimes + nickles + pennies
-    resources['money'] = float(resources['money']) + float(round_money(total_pay))
+    resources['money'] = float(resources['money']) + float(
+        round_money(total_pay))
     # Go back to process_payment
     return total_pay
-    
+
 
 def print_menu():
     '''Prints the drink menu.'''
@@ -214,13 +230,14 @@ def print_menu():
     print(f"     Latte: ${MENU['latte']['cost']}0")
     print(f"Cappuccino: ${MENU['cappuccino']['cost']}0")
     print("")
-  
+
 
 def take_order():
     '''Asks for the order from the customer.'''
     print_menu()
     # Hidden are the options [r] for report, [off] for power down, and [add] for adding resources.
-    order = input("​What would you like? [E]spresso / [L]atte / [C]appuccino: ").lower()
+    order = input(
+        "​What would you like? [E]spresso / [L]atte / [C]appuccino: ").lower()
     # Comment above and uncomment below for quick testing.
     # order = "e"
     # Check to see if we have enough resources.
@@ -232,22 +249,30 @@ def make_drink(drink_type):
     print(f"Making your Drink!")
     sleep(3)
     if drink_type == 'e':
-        resources['water'] = resources['water'] - MENU['espresso']["ingredients"]['water']
-        resources['coffee'] = resources['coffee'] - MENU['espresso']["ingredients"]['coffee']
+        resources['water'] = resources['water'] - MENU['espresso'][
+            "ingredients"]['water']
+        resources['coffee'] = resources['coffee'] - MENU['espresso'][
+            "ingredients"]['coffee']
         print("Enjoy Your Espresso! ☕")
         sleep(5)
         take_order()
     if drink_type == 'l':
-        resources['water'] = resources['water'] - MENU['latte']["ingredients"]['water']
-        resources['milk'] = resources['milk'] - MENU['latte']["ingredients"]['milk']
-        resources['latte'] = resources['coffee'] - MENU['latte']["ingredients"]['coffee']
+        resources['water'] = resources['water'] - MENU['latte']["ingredients"][
+            'water']
+        resources[
+            'milk'] = resources['milk'] - MENU['latte']["ingredients"]['milk']
+        resources['latte'] = resources['coffee'] - MENU['latte'][
+            "ingredients"]['coffee']
         print("Enjoy Your Latte! ☕")
         sleep(5)
         take_order()
     if drink_type == 'c':
-        resources['water'] = resources['water'] - MENU['cappuccino']["ingredients"]['water']
-        resources['milk'] = resources['milk'] - MENU['cappuccino']["ingredients"]['milk']
-        resources['latte'] = resources['coffee'] - MENU['cappuccino']["ingredients"]['coffee']
+        resources['water'] = resources['water'] - MENU['cappuccino'][
+            "ingredients"]['water']
+        resources['milk'] = resources['milk'] - MENU['cappuccino'][
+            "ingredients"]['milk']
+        resources['latte'] = resources['coffee'] - MENU['cappuccino'][
+            "ingredients"]['coffee']
         print("Enjoy Your Cappuccino! ☕")
         sleep(5)
         take_order()
@@ -256,11 +281,10 @@ def make_drink(drink_type):
 clear_screen()
 take_order()
 
-
 # DONE Prompt User with "​What would you like? ( [E]spresso/[L]atte/[C]appuccino )"
 # DONE create "off" command to shut it down
 # DONE cretae "report" command to print report
 # DONE check resources before making coffee
 # DONE process coins for the payment and give change or tell not enough for drink
 # DONE deduct from resources when making coffee
-# DONE Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If 
+# DONE Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If
---
+++ Hirst Painting - Day 19/main.py
@@ -0,0 +1,56 @@
+# import colorgram
+
+# colors = colorgram.extract('image.jpg', 30)
+# color_list = []
+
+# for i in colors:
+#     r = i.rgb.r
+#     g = i.rgb.g
+#     b = i.rgb.b
+#     color_list.append((r, g, b)) 
+
+# print(color_list)
+
+
+import turtle as t
+from turtle import Screen
+import random
+
+tim = t.Turtle()
+my_screen = Screen()
+my_screen.delay(1)
+my_screen.screensize(100000, 100000, bg="black")
+tim.shape("circle")
+tim.speed(0)
+my_screen.colormode(255)
+tim.color("white")
+
+EAST = 0
+NORTH = 90
+WEST = 180
+SOUTH = 270
+
+color_list = [(236, 35, 108), (145, 28, 64), (239, 75, 35), (6, 148, 93), (232, 238, 234), (231, 168, 40), (184, 158, 
+46), (44, 191, 233), (27, 127, 195), (126, 193, 74), (253, 223, 0), (85, 28, 93), (173, 36, 97), (246, 219, 44), (44, 172, 112), (215, 130, 165), (215, 56, 
+27), (235, 164, 191), (156, 24, 23), (21, 188, 230), (238, 169, 157), (162, 210, 182), (138, 210, 232), (0, 123, 54), (88, 130, 182), (180, 187, 211)] 
+
+
+def dot_machine(spacing, shape_size, rows, columns):
+    tim.shapesize(shape_size)
+    tim.penup()
+    tim.setposition(x=-250, y=-250)
+    tim.hideturtle()
+    
+    for _ in range(rows):
+        for x in range(columns):
+            tim.color(random.choice(color_list))
+            tim.setheading(EAST)
+            tim.stamp()
+            # tim.write(x)
+            tim.forward(spacing)
+        tim.backward(columns * spacing)
+        tim.setheading(NORTH)
+        tim.forward(spacing)
+     
+dot_machine(50, 1, 10, 10)
+my_screen.exitonclick()
